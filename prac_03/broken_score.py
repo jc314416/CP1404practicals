713ef12932cfb0bb9f995a5414355fa2be311c0c
jc314416 <30332529+jc314416@users.noreply.github.com>
2019-08-27 09:23:28 +1000

    Remove idea file

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -25,16 +25,16 @@ def determine_result(score):
 
 
 main()
-"""
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-        print("Excellent")
-if score < 50:
-    print("Bad")
-"""
+
+# if score < 0:
+#     print("Invalid score")
+# else:
+#     if score > 100:
+#         print("Invalid score")
+#     if score > 50:
+#         print("Passable")
+#     if score > 90:
+#         print("Excellent")
+# if score < 50:
+#     print("Bad")
+
