bfb1f30cb74afef40420c788132ce5e40d9fb97e
jc314416 <30332529+jc314416@users.noreply.github.com>
2019-10-19 12:05:25 +1000

    Add completed sort_files_1.py file

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+
+
+def main():
+    os.chdir('FilesToSort')
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+    for filename in os.listdir('.'):
+        directory_name = filename.split(".")[1]
+        print(directory_name)
+        try:
+            os.mkdir(directory_name)
+        except FileExistsError:
+            pass
+        shutil.move(filename, directory_name + "/" + filename)
+
+
+main()
